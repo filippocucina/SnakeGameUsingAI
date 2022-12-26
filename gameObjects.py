790d818145c332e3fa0e497ea7f34c82d47cc888
Filippo <filippocucina5@gmail.com>
2022-12-25 20:21:03 -0400

    One Copy file deleted

--- gameObjects.py
+++ gameObjects.py
@@ -21,8 +21,8 @@ class Snake(Entity):
         self.color_blue = int(color_blue)
     
     
-    def draw_snake(self):
-        pygame.draw.rect(Window, (self.color_red, self.color_green, self.color_blue), )
+    def render_snake(self):
+        pygame.draw.rect(Window, (self.color_red, self.color_green, self.color_blue), self.positionX, self.positionY, self.width, self.height)
         pass
     
     
@@ -31,16 +31,26 @@ class Snake(Entity):
 
     
     def destroy_snake(self):
+        self.kill()
         pass
 
 
 class Apple(Entity):
 
-    def __init__(self):
+    def __init__(self, color_red, color_green, color_blue):
+        Entity.__init__(self, self.positionX, self.positionY, self.width, self.height)
+        self.color_red = int(color_red)
+        self.color_green = int(color_green)
+        self.color_blue = int(color_blue)
         pass
 
 
-    def draw_apple(self):
+    def render_apple(self):
+        pygame.draw.rect(Window, (self.color_red, self.color_green, self.color_blue), self.positionX, self.positionY, self.width, self.height)
+        pass
+
+
+    def random_position(self):
         pass
 
 
